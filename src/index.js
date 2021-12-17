71fe127085c6fae01bd0761b195decb444cd3270
Elias Emanuel Copa <elias.copa-non-empl@moodys.com>
2021-12-17 14:18:32 -0300

    feat: script

---
+++ src/index.js
@@ -0,0 +1,74 @@
+const yargs = require("yargs");
+const csv = require("csv-parser");
+const fs = require("fs");
+const axios = require("axios").default;
+
+const argv = yargs.argv;
+const filePath = argv.path;
+
+const emcInstance = axios.create({
+	baseURL: `${process.env.EMC_DOMAIN_URL}/api/`,
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+		authorizationtoken: `Bearer ${process.env.EMC_API_TOKEN}`,
+	},
+});
+
+
+const mapDataToUser = (data) => ({
+	firstName: data["FirstName"],
+	lastName: data["LastName"],
+	userName: data["UserName"],
+	email: data["UserName"],
+	accountId: data["AccountId"],
+});
+
+const uploadUser = async (data) => {
+	try {
+		const roles = data.RolesIds.split(",");
+
+		const userData = mapDataToUser({ ...data });
+		
+        await createEMCUser(userData);
+
+		const assignRolesToUserResult = (await Promise.allSettled(
+			roles.map(async (roleId) => {
+				await assignRoleToUser(userData.email, roleId?.trim());
+			})
+		)).filter(result => result.status != "fulfilled");
+
+        console.dir(assignRolesToUserResult);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const createEMCUser = async (user) => {
+	try {
+		await emcInstance.post(`/users`, user);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+const assignRoleToUser = async (roleId, userId) => {
+	try {
+		await emcInstance.put(`applicationroles/${roleId}/users/${userId}`);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+fs.createReadStream(filePath)
+	.pipe(csv())
+	.on("data", (row) => {
+		console.log(row);
+		uploadUser(row);
+	})
+	.on("end", () => {
+		console.log("CSV file successfully processed");
+	})
+	.on("error", (err) => {
+		console.error(err);
+	});
